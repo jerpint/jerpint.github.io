edeff41f3f4a0fbfcc55274d7e44500cb79d61b6
Jeremy Pinto <jerpint@gmail.com>
2022-02-25 00:27:30 -0500

    vortex math post

---
+++ _posts/2022-02-24-mathologer-challenge.md
@@ -0,0 +1,36 @@
+---
+title: "Vortex Math"
+date: 2022-02-24T00:00:00-05:00
+excerpt: "Implementing the vortex math using javascript."
+categories:
+  - blog
+tags:
+  - javascript
+  - p5
+classes: wide
+header:
+  og_image: /assets/images/vortex.png
+---
+
+Mathologer made a really neat video on Vortex Math and proposed to implement one as a coding challenge [in his video](https://www.youtube.com/embed/6ZrO90AI0c8?start=1097)
+
+Here is my implementation... Enjoy!
+
+<!-- <iframe src="https://editor.p5js.org/jerpint/full/ta-N2RUpz"></iframe> -->
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.jsdelivr.net/npm/p5@1.3.1/lib/p5.js"></script>
+    <script src="/assets/vortexmath.js"></script>
+  </head>
+  <body>
+   <main>
+
+
+
+  <div id="sketchHolder"></div>
+
+   </main>
+  </body>
+</html>
---
+++ assets/vortexmath.js
@@ -0,0 +1,114 @@
+let width = 600;
+let height = 600;
+let circleX = width / 2;
+let circleY = height / 2 - 30;
+let radius = width / 2.5;
+
+
+"use strict";
+
+// monokai colorscheme for the lines
+let colors = [
+  [121, 121, 121], // grey
+  [229, 181, 103], // yellow
+  [180, 210, 115], // green
+  [232, 125, 62], // orange
+  [158, 134, 200], // purple
+  [176, 82, 121], // pink
+  [108, 153, 187], // blue
+  [214, 214, 214], // white
+];
+
+function setup() {
+  canv = createCanvas(width, height).parent(sketchHolder);
+
+  sliderModulus = createSlider(2, 300, 111);
+  sliderMultiplier = createSlider(1, 100, 2);
+
+  // sliderModulus.position(0, 110);
+  sliderModulus.parent("sketchHolder");
+  sliderModulus.position(0, height + 100, 'absolute');
+  // SliderModulus.position(sliderModulus.x * 2 + sliderModulus.width + 10, 25)
+  sliderModulus.style("width", "200px");
+  //
+  sliderMultiplier.parent("sketchHolder");
+  sliderMultiplier.position(0, height+120, 'absolute');
+  sliderMultiplier.style("width", "200px");
+
+}
+
+function duplicateExists(arr) {
+  return new Set(arr).size !== arr.length;
+}
+
+function getPath(multiplier, modulus, start) {
+  let path = [start];
+  let currentNumber = path[0];
+  // break once we found a duplicate, it just loops on itself afterwards
+  while (path.length < 2 || !duplicateExists(path)) {
+    currentNumber = currentNumber * multiplier;
+    currentNumber = currentNumber % modulus;
+    path.push(currentNumber);
+  }
+  return path;
+}
+
+function drawPath(path, color) {
+  let modulus = sliderModulus.value();
+  let multiplier = sliderMultiplier.value();
+
+  for (let i = 0; i < path.length - 1; i++) {
+    angle0 = (path[i] * 2 * Math.PI) / modulus + Math.PI;
+    angle1 = (path[i + 1] * 2 * Math.PI) / modulus + Math.PI;
+
+    let x0 = -radius * sin(angle0) + circleX;
+    let y0 = radius * cos(angle0) + circleY;
+
+    let x1 = -radius * sin(angle1) + circleX;
+    let y1 = radius * cos(angle1) + circleY;
+
+    stroke(color);
+    strokeWeight(1);
+
+    line(x0, y0, x1, y1);
+  }
+}
+function draw() {
+  background(220);
+  let modulus = sliderModulus.value();
+  let multiplier = sliderMultiplier.value();
+ 
+  stroke("white");
+
+  text(
+    "Modulus: " + sliderModulus.value(),
+    sliderModulus.x * 2 + sliderModulus.width + 10,
+    height - 40 
+  );
+  text(
+    "Multiplier: " + sliderMultiplier.value(),
+    sliderMultiplier.x * 2 + sliderMultiplier.width + 10,
+    height - 10
+  );
+
+  // Draw the circle and evenly spaced dots
+  circle(circleX, circleY, radius * 2);
+  for (let i = 0; i < modulus; i++) {
+    angle = (i * 2 * Math.PI) / modulus + Math.PI;
+    let x = -radius * sin(angle) + circleX;
+    let y = radius * cos(angle) + circleY;
+    stroke("black");
+    strokeWeight(3);
+    point(x, y);
+  }
+
+  // don't revisit a point if not necessary
+  let visited = [];
+  for (let start = 0; start < modulus; start++) {
+    if (!visited.includes(start)) {
+      path = getPath(multiplier, modulus, start);
+      drawPath(path, colors[start % colors.length]);
+      visited = visited.concat(path);
+    }
+  }
+}
