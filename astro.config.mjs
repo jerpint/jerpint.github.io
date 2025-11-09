53f8f3b84b8a37dd01ec19e6dff0bcb48c2434df
Jeremy Pinto <jerpint@gmail.com>
2025-11-09 15:18:24 -0500

    get actions ready

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,6 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-});
+  site: 'https://jerpint.github.io',
+})
